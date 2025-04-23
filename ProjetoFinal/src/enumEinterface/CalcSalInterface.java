cea166d1c69a7dac8e5ee9a586afaee2055cf05b
colosso97 <marcelovidal.ml@gmail.com>
2025-04-22 21:20:49 -0300

    resolvendo erros de merge

--- ProjetoFinal/src/enumEinterface/CalcSalInterface.java
+++ ProjetoFinal/src/enumEinterface/CalcSalInterface.java
@@ -1,11 +1,6 @@
 package enumEinterface;
 
 public interface CalcSalInterface {
-
-<<<<<<< HEAD
 	public void atualizarDesconto();
 	
-=======
-
->>>>>>> 20e41a9 (breve description)
 }
--- ProjetoFinal/src/module-info.java
+++ ProjetoFinal/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ProjetoFinal {
+	requires java.sql;
 }
