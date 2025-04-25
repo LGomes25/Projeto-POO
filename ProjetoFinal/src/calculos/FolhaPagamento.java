ef3f89e71edb484fec5f4c4c12026c8126cdb6f2
lgomes25 <leomybi@gmail.com>
2025-04-24 21:44:19 -0300

    Calculo IR + INSS

--- ProjetoFinal/src/calculos/FolhaPagamento.java
+++ ProjetoFinal/src/calculos/FolhaPagamento.java
@@ -11,7 +11,7 @@ public class FolhaPagamento extends Funcionario implements CalcSalInterface {
 	// atributos
 	private Integer id;
 	private LocalDate dataPagamento;
-	private int contdependente2; //->contador dependentes
+	private int contdependente2; // ->contador dependentes
 
 	// construtor
 	public FolhaPagamento(Integer id, String nome, String cpf, LocalDate dataNascimento, Double salarioBruto) {
@@ -19,6 +19,12 @@ public class FolhaPagamento extends Funcionario implements CalcSalInterface {
 		this.dataPagamento = LocalDate.now();
 		this.contdependente2 = 0;
 
+	}// toString
+
+	@Override
+	public String toString() {
+		return "Nome: " + nome + " CPF: " + cpf + " D. pgt.: " + dataPagamento + " Desc. INSS: " + descontoInss
+				+ " Desc. IR: " + descontoIr + " Sal. Liq.: " + salarioLiquido;
 	}
 
 	@Override
@@ -30,20 +36,19 @@ public class FolhaPagamento extends Funcionario implements CalcSalInterface {
 		setDescontoIr(descontoir);
 		setSalarioLiquido(salarioliquidofinal);
 	}
-	
-	
+
 	private Double calcularINSS() {
-		if (getSalarioBruto() <= 1518.0) {
-			Double faixa1 = Math.round(((getSalarioBruto() * 0.075) - 0.) * 100.0) / 100.0;
+		if (salarioBruto <= 1518.0) {
+			Double faixa1 = Math.round(((salarioBruto * 0.075) - 0.) * 100.0) / 100.0;
 			return faixa1;
-		} else if (getSalarioBruto() >= 1518.01 & getSalarioBruto() <= 2793.88) {
-			Double faixa2 = Math.round(((getSalarioBruto() * 0.09) - 22.77) * 100.0) / 100.0;
+		} else if (salarioBruto >= 1518.01 & salarioBruto <= 2793.88) {
+			Double faixa2 = Math.round(((salarioBruto * 0.09) - 22.77) * 100.0) / 100.0;
 			return faixa2;
-		} else if (getSalarioBruto() >= 2793.89 & getSalarioBruto() <= 4190.83) {
-			Double faixa3 = Math.round(((getSalarioBruto() * 0.12) - 106.60) * 100.0) / 100.0;
+		} else if (salarioBruto >= 2793.89 & salarioBruto <= 4190.83) {
+			Double faixa3 = Math.round(((salarioBruto * 0.12) - 106.60) * 100.0) / 100.0;
 			return faixa3;
-		} else if (getSalarioBruto() >= 4190.84 & getSalarioBruto() <= 8157.41) {
-			Double faixa4 = Math.round(((getSalarioBruto() * 0.14) - 190.42) * 100.0) / 100.0;
+		} else if (salarioBruto >= 4190.84 & salarioBruto <= 8157.41) {
+			Double faixa4 = Math.round(((salarioBruto * 0.14) - 190.42) * 100.0) / 100.0;
 			return faixa4;
 		} else {
 			Double limite = Math.round((8157.41 * 0.14) * 100.0) / 100.0;
@@ -53,46 +58,44 @@ public class FolhaPagamento extends Funcionario implements CalcSalInterface {
 			} else {
 				faixa5 = limite;
 			}
-			
 			return faixa5;
 		}
 	}
-	
 
 	private Double calcularIR() {
-		
+
 		Double descDep = contdependente2 * 189.59;
-		Double salBase = salarioBruto - descDep - 951.62;
-		
-		
+		Double salBase = salarioBruto - descDep - this.calcularINSS();
+
 		if (salBase <= 2259.20) {
 			Double faixair0 = 0.0;
 			return faixair0;
-		} else if (salBase <= 2826.65){
-			Double faixair1 = (salBase * 0.075) - 169.44;
+		} else if (salBase <= 2826.65) {
+			Double faixair1 = Math.round(((salBase * 0.075) - 169.44) * 100.0) / 100.0;
 			return faixair1;
-		} else if(salBase <= 3751.05) {
-			Double faixair2 = (salBase * 0.15) - 381.44;
+		} else if (salBase <= 3751.05) {
+			Double faixair2 = Math.round(((salBase * 0.15) - 381.44) * 100.0) / 100.0;
 			return faixair2;
-		} else if(salBase <= 4664.68) {
-			Double faixair3 = (salBase * 0.225) - 662.77;
+		} else if (salBase <= 4664.68) {
+			Double faixair3 = Math.round(((salBase * 0.225) - 662.77) * 100.0) / 100.0;
 			return faixair3;
 		} else {
-			Double faixair4 = (salBase * 0.275) - 896.00;
+			Double faixair4 = Math.round(((salBase * 0.275) - 896.00) * 100.0) / 100.0;
 			return faixair4;
-		}	
+		}
 	}
-	 private Double calcularSalarioLiquido() {
-		 Double salarioliquido = Math.round((getSalarioBruto() - calcularINSS() - calcularIR()) * 100.0) / 100.0;
-		 return salarioliquido;
-	 }
+
+	private Double calcularSalarioLiquido() {
+		Double salarioliquido = Math.round((getSalarioBruto() - calcularINSS() - calcularIR()) * 100.0) / 100.0;
+		return salarioliquido;
+	}
 
 	public Double calcularDescontoDependentes() {
 		Double descontoDependente = ParentescoEnum.FILHO.getDesconto();
 		return descontoDependente;
 	}
 
-	//Getters Setters
+	// Getters Setters
 	public Integer getId() {
 		return id;
 	}
@@ -117,5 +120,5 @@ public class FolhaPagamento extends Funcionario implements CalcSalInterface {
 		this.contdependente2 = contdependente2;
 		System.out.println(contdependente2);
 	}
-	
+
 }
